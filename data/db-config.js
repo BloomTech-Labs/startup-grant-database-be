a9a934d91da8605c3634be2fb5ce0b5abc4bd136
yusufnafey <yusufnafey@yahoo.com>
2019-10-01 15:07:57 -0700

    changed development server
    >Co-authored-by: Reed <reedtjames1234@gmail.com>
    >Co-authored-by: Phil  <phil7j@protonmail.com>
    >Co-authored-by: Michael <mhguitar1997@gmail.com>

--- data/db-config.js
+++ data/db-config.js
@@ -1,7 +1,20 @@
 const knex = require("knex");
 
+// const knex = require('knex')({
+//     client: pg,
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         password: 'password',
+//         database: 'grants'
+//     }
+// })
+
+
 const config = require("../knexfile.js");
 
 const dbEnv = process.env.DB_ENV || "development";
+
+// module.exports = knex
 
 module.exports = knex(config[dbEnv]);
--- knexfile.js
+++ knexfile.js
@@ -10,22 +10,30 @@ const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/grants.db3"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    },
+    client: "pg",
+    connection: dbConnection,
     migrations: {
-      directory: "./data/migrations"
+      directory: __dirname + "/data/migrations"
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: __dirname + "/data/seeds"
     }
+    // client: "sqlite3",
+    // useNullAsDefault: true,
+    // connection: {
+    //   filename: "./data/grants.db3"
+    // },
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     conn.run("PRAGMA foreign_keys = ON", done);
+    //   }
+    // },
+    // migrations: {
+    //   directory: "./data/migrations"
+    // },
+    // seeds: {
+    //   directory: "./data/seeds"
+    // }
   },
 
   production: {
