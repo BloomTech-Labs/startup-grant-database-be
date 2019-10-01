8eb7a5235d4e6b3719e112b95e81af964e698f55
yusufnafey <yusufnafey@yahoo.com>
2019-10-01 15:11:16 -0700

    changes development server
    >Co-authored-by: Reed <reedtjames1234@gmail.com>
    >Co-authored-by: Phil  <phil7j@protonmail.com>
    >Co-authored-by: Michael <mhguitar1997@gmail.com>

--- data/db-config.js
+++ data/db-config.js
@@ -11,7 +11,7 @@ const knex = require("knex");
 // })
 
 
-const config = require("../knexfile.js");
+const config = require("../knexfile.js")
 
 const dbEnv = process.env.DB_ENV || "development";
 
