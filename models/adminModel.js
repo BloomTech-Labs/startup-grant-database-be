a3f66fc0950dd2b1964ec6cbead060a0f2c73609
Philip Johnson <phil7j@gmail.com>
2019-10-01 10:08:30 -0700

    added admin model to include suggestions with grants

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,78 +1,33 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-  //   get,
-  putReviewedGrant,
+  getPendingGrants,
+  putPendingGrants,
   find
 };
 
-function getReviewedGrant() {
+function getPendingGrants() {
   return db("grants").where({ is_reviewed });
 }
 
-function putReviewedGrant(changes, id) {
+function putPendingGrants(changes, id) {
   return db("grants")
     .where({ id })
     .update({ changes });
 }
 
-// function find() {
-//   return db("requests as r")
-//     .innerJoin("grants as g", "r.grant_id", "=", "g.id")
-//     .select("r.id, r.suggestion");
-// }
-
-// Trying a left join
-// function find() {
-//   return db("grants")
-//     .select("*")
-//     .from("grants as g")
-//     .leftJoin("requests as r", "r.grant_id", "=", "g.id");
-// }
-
-//#####This works!################
 function find() {
-  return db("requests")
-    .from("requests as r")
-    .leftJoin("grants as g", "r.grant_id", "=", "g.id")
-    .select("*", "g.id");
+  return db("grants").then(grants => {
+    let currentSuggestions;
+    let newGrants;
+    return db("requests").then(suggestions => {
+      return (newGrants = grants.map(grant => {
+        currentSuggestions = suggestions.filter(node => {
+          console.log(node.grant_id);
+          return grant.id === node.grant_id;
+        });
+        return { ...grant, requests: currentSuggestions };
+      }));
+    });
+  });
 }
-//#####This works!################
-
-// function find() {
-//   return db("requests")
-//     .select("*")
-//     .from("requests as r")
-//     .join("grants as g", "r.grant_id", "g.id")
-//     .where("r.grant_id", "g.id");
-// }
-
-// function findSteps(id) {
-//     return db("steps")
-//       .innerJoin("schemes", "steps.scheme_id", "=", "schemes.id")
-//       .select(
-//         "steps.id",
-//         "schemes.scheme_name",
-//         "steps.step_number",
-//         "steps.instructions"
-//       )
-//       .where({ scheme_id: id });
-//   }
-
-// function find() {
-//   return db("grants").then(grants => {
-//     // console.log("grants", grants);
-//     return db("requests").then(suggestions => {
-//       // console.log("suggestions", suggestions);
-//       let newGrants = grants.map(grant => {
-//         let currentSuggestions = suggestions.filter(
-//           node => grant.id === node.grant_id
-//         );
-//         grant.requests = currentSuggestions;
-//         return currentSuggestions;
-//       });
-//       console.log("NEW GRANTS", newGrants);
-//       return newGrants;
-//     });
-//   });
-// }
