6c21e9a543c81b630d6399e5bf674bbb30c487cc
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-11-14 17:47:28 -0700

    Added third step for token authorization roles. Tested locally with auth0 test token and working. User tokens not working because may be blocked by paid features

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const server = express();
+const bodyParser = require('body-parser');
 
 const grantRouter = require("./routers/grantRouter.js");
 const userRouter = require("./routers/userRouter.js");
@@ -9,19 +10,25 @@ const adminRouter = require("./routers/adminRouter.js");
 
 // Auth0's authentication for all users
 const middleware = require("./auth/middleware.js");
+const jwtAuthz = require("express-jwt-authz"); //jwtAuthz(['batch:upload'])
 
 // Our own custom middleware to check if user is admin
-const adminMiddleware = require("./auth/adminMiddleware.js");
+// const adminMiddleware = require("./auth/adminMiddleware.js");
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
+// enable the use of request body parsing middleware
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Routes
 server.use("/api/grants", grantRouter);
 server.use("/user", userRouter);
-// Implement middleware on our protected admin route
-server.use("/api/admin", middleware, adminMiddleware, adminRouter);
+// Implement middleware on our protected admin route This is working with test token globally!!!
+server.use("/api/admin", middleware, jwtAuthz(['get:grants'] , { checkAllScopes: true }), adminRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ server: "running" });
