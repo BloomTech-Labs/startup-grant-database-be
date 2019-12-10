835080e293b18945d74cb57d1c82e3f6ef7eadda
Ben <bigriffin94@outlook.com>
2019-12-10 10:00:55 -0800

    Refactored Table + columns + seed data Removed 2 columns from GrantTable we are no longer using Adjusted Seed Data for testing migration and seeding was successful
    Co-authored-by: Kristin <kristinbarr@gmail.com>
    Co-authored-by: Daniel <daniel.jones2000@gmail.com>
    Co-authored-by: Benita <nicole.beamer@icloud.com>
    Co-authored-by: Ben <bigriffin94@gmail.com>
    Co-authored-by: Mario <mario.amaya.web@gmail.com>

--- data/migrations/20190912154411_bootstrap.js
+++ data/migrations/20190912154411_bootstrap.js
@@ -1,23 +1,23 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable("grants", tbl => {
-      tbl.increments();
-      tbl.string("competition_name", 255);
-      tbl.string("type", 255);
-      tbl.string("area_focus", 255);
-      tbl.string("sponsoring_entity", 255);
-      tbl.string("website", 500);
-      tbl.date("most_recent_application_due_date");
-      tbl.integer("amount");
-      tbl.string("amount_notes", 1000);
-      tbl.string("geographic_region", 255);
-      tbl.string("domain_areas", 1000);
-      tbl.string("target_entrepreneur_demographic", 255);
-      tbl.string("notes", 5000);
-      tbl.boolean("early_stage_funding");
-      tbl.boolean("is_reviewed");
-      tbl.boolean("has_requests");
-      tbl.date("details_last_updated");
+      tbl.increments(); //Keep
+      tbl.string("competition_name", 500); //Keep
+      // tbl.string("type", 255);
+      tbl.string("area_focus", 255); //Keep
+      tbl.string("sponsoring_entity", 255); //Keep
+      tbl.string("website", 500); //Keep
+      tbl.date("most_recent_application_due_date"); //Keep
+      tbl.integer("amount"); //Keep
+      tbl.string("amount_notes", 1000); //Keep
+      tbl.string("geographic_region", 255); //Keep
+      // tbl.string("domain_areas", 1000); 
+      tbl.string("target_entrepreneur_demographic", 255); //Keep
+      tbl.string("notes", 5000); //Keep
+      tbl.boolean("early_stage_funding"); //Keep
+      tbl.boolean("is_reviewed"); //Keep
+      tbl.boolean("has_requests"); //Keep
+      tbl.date("details_last_updated"); //Keep
     })
     .createTable("requests", tbl => {
       tbl.increments();
