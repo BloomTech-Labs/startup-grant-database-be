c9f9d2d80da3b4cc53ee891b6fe020d967d62683
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-12-12 13:11:32 -0700

    updating for user roles

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ server.use(bodyParser.urlencoded({
 server.use("/api/grants", grantRouter);
 server.use("/user", userRouter);
 // Implement middleware on our protected admin route This is working with test token globally!!!
-server.use("/api/admin", middleware, jwtAuthz(['get:adminLocal'] , { checkAllScopes: true }), adminRouter);
+server.use("/api/admin", middleware, jwtAuthz(['get:adminLocal', 'get:adminProduction', 'get:adminStaging'] , { checkAllScopes: true }), adminRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ server: "running" });
