49936e41a56023038e70955d30d356ead038b49e
Ben <bigriffin94@outlook.com>
2020-01-09 13:05:14 -0800

    New testing branch for heorku issues

--- data/seeds/001-grants.js
+++ data/seeds/001-grants.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("grants")
+  return knex("grants").truncate()
     .then(function() {
       // Inserts seed entries
       return knex("grants").insert([
