092eaeb9f3723852a83d75786d8bb2e22c8b3874
Donald Whitely <dswhitely1@gmail.com>
2020-02-10 13:08:31 -0500

    Added Base Model

---
+++ models/base.model.js
@@ -0,0 +1,27 @@
+const db = require('../data/db-config');
+
+class Model {
+    constructor(name) {
+        this.name = name;
+    }
+
+    find() {
+        return db(this.name);
+    }
+
+    findBy(filter) {
+        return db(this.name).where(filter);
+    }
+
+    add(newItem) {
+        return db(this.name).insert(newItem).returning('*')
+    }
+
+    update(id, updateItem) {
+        return db(this.name).where({id}).update(updateItem).returning('*')
+    }
+
+    remove(id) {
+        return db(this.name).where({id}).del();
+    }
+}
