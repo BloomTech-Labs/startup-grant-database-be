3ef4ed0a94c763540d364f497042c2120e41c86d
spenclark <spenclark@hotmail.com>
2020-07-06 13:44:04 -0700

    defualt to 0

--- data/migrations/20190912154411_bootstrap.js
+++ data/migrations/20190912154411_bootstrap.js
@@ -1,31 +1,31 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("grants", tbl => {
+  return knex.schema.createTable('grants', tbl => {
     tbl.increments(); // Keep
-    tbl.string("competition_name", 500); // Keep
+    tbl.string('competition_name', 500); // Keep
     // tbl.string("type", 255);
-    tbl.string("area_focus", 255); // Keep
-    tbl.string("sponsoring_entity", 255); // Keep
-    tbl.string("website", 500); // Keep
-    tbl.string("most_recent_application_due_date"); // Keep
-    tbl.integer("amount").defaultTo(0); // Keep
-    tbl.string("amount_notes", 1000); // Keep
-    tbl.string("geographic_region", 255); // Keep
-    tbl.string("country");
+    tbl.string('area_focus', 255); // Keep
+    tbl.string('sponsoring_entity', 255); // Keep
+    tbl.string('website', 500); // Keep
+    tbl.string('most_recent_application_due_date'); // Keep
+    tbl.integer('amount').defaultTo(0); // Keep
+    tbl.string('amount_notes', 1000); // Keep
+    tbl.string('geographic_region', 255); // Keep
+    tbl.string('country');
     // tbl.string("domain_areas", 1000);
-    tbl.string("target_entrepreneur_demographic", 255); // Keep
-    tbl.string("notes", 5000); // KeepOptions for --register-aws
+    tbl.string('target_entrepreneur_demographic', 255); // Keep
+    tbl.string('notes', 5000); // KeepOptions for --register-aws
 
-    tbl.boolean("early_stage_funding"); // Keep
-    tbl.boolean("is_reviewed"); // Keep
-    tbl.boolean("has_requests"); // Keep
-    tbl.string("details_last_updated"); // Keep
-    tbl.string("domain_areas", 500);
-    tbl.string("type");
-    tbl.string("logo", 255).defaultTo(null);
-    tbl.boolean("use_logo").defaultTo(null);
+    tbl.boolean('early_stage_funding'); // Keep
+    tbl.boolean('is_reviewed'); // Keep
+    tbl.boolean('has_requests'); // Keep
+    tbl.string('details_last_updated'); // Keep
+    tbl.string('domain_areas', 500);
+    tbl.string('type');
+    tbl.string('logo', 255).defaultTo(null);
+    tbl.boolean('use_logo').defaultTo(null);
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("grants");
+  return knex.schema.dropTableIfExists('grants');
 };
